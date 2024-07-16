338d4fc7d1e8372d5377d48371fd1d6d34299061
Makar Timofeev <makar.timofeev.1990@gmail.com>
2024-07-16 15:08:37 +0300

    script_logs argMAx added

--- task2/02-create.sql
+++ task2/02-create.sql
@@ -1,12 +1,13 @@
 CREATE TABLE script_logs (
-    timestamp DateTime64(6),
+    script_id String,
+    start_timestamp DateTime64(6),
+    end_timestamp DateTime64(6),
     user String,
     communication_number Int64,
     communication_id Nullable(Float64),
     mrf Nullable(String),
     client_mrf String,
     crm_departament Nullable(String),
-    script_id String,
     script_name Nullable(String),
     script_owner Nullable(String),
     current_script_owner Nullable(String),
@@ -18,4 +19,4 @@ CREATE TABLE script_logs (
     communication_detail Nullable(String),
     communication_result Nullable(String)
 ) ENGINE = MergeTree()
-ORDER BY timestamp;
+ORDER BY (script_id);
--- task2/03-insert.sql
+++ task2/03-insert.sql
@@ -1,21 +1,24 @@
 INSERT INTO script_logs
 SELECT
-    timestamp,
-    user,
-    communication_number,
-    communication_id,
-    mrf,
-    client_mrf,
-    crm_departament,
     script_id,
-    script_name,
-    script_owner,
-    current_script_owner,
-    script_responsible,
-    current_script_responsible,
-    nullIf(JSONExtractString(parameters, 'ACCOUNT_NUMBER'), '') AS account_number,
-    nullIf(JSONExtractString(parameters, 'CALLER_ID'), '') AS caller_id,
-    nullIf(JSONExtractString(parameters, 'COMMUNICATION_THEME'), '') AS communication_theme,
-    nullIf(JSONExtractString(parameters, 'COMMUNICATION_DETAIL'), '') AS communication_detail,
-    nullIf(JSONExtractString(parameters, 'COMMUNICATION_RESULT'), '') AS communication_result
-FROM script_logs_raw;
+    MIN(timestamp) AS start_timestamp,
+    MAX(timestamp) AS end_timestamp,
+    argMax(user, timestamp) AS user,
+    argMax(communication_number, timestamp) AS communication_number,
+    argMax(communication_id, timestamp) AS communication_id,
+    argMax(mrf, timestamp) AS mrf,
+    argMax(client_mrf, timestamp) AS client_mrf,
+    argMax(crm_departament, timestamp) AS crm_departament,
+    argMax(script_name, timestamp) AS script_name,
+    argMax(script_owner, timestamp) AS script_owner,
+    argMax(current_script_owner, timestamp) AS current_script_owner,
+    argMax(script_responsible, timestamp) AS script_responsible,
+    argMax(current_script_responsible, timestamp) AS current_script_responsible,
+    argMax(nullIf(JSONExtractString(parameters, 'ACCOUNT_NUMBER'), ''), timestamp) AS account_number,
+    argMax(nullIf(JSONExtractString(parameters, 'CALLER_ID'), ''), timestamp) AS caller_id,
+    argMax(nullIf(JSONExtractString(parameters, 'COMMUNICATION_THEME'), ''), timestamp) AS communication_theme,
+    argMax(nullIf(JSONExtractString(parameters, 'COMMUNICATION_DETAIL'), ''), timestamp) AS communication_detail,
+    argMax(nullIf(JSONExtractString(parameters, 'COMMUNICATION_RESULT'), ''), timestamp) AS communication_result
+FROM script_logs_raw
+WHERE script_id NOT IN (SELECT script_id FROM script_logs)
+GROUP BY script_id;
